7804be8da0269819d04e2e0148e3ac9e1f059a8b
John Tucker <johntucker@Johns-MacBook-Pro.local>
2019-11-21 05:01:18 -0500

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,25 @@
 /* eslint-disable no-console */
 {
+  const test = (x: number): number => x + 1;
   let r;
   console.log(`r: ${r}`); // undefined
-  {
-    const x = { hello: 'world' };
-    r = x;
-  }
-  console.log(`r: ${r.hello}`); // world
+  // r = r + 1; // CANNOT BE APPLIED
+  const s = test(r);
+  console.log(`s: ${s}`); // NaN
 }
 
 {
   let r;
   r = 5;
   r += 1;
-  console.log(`r: ${r}`); // 5
-  {
-    const x = { hello: 'world' };
-    r = x;
-  }
-  console.log(`r: ${r.hello}`); // world
-}
-
-interface Hello {
-  hello: string;
+  console.log(`r: ${r}`); // 6
+  r = 'abc';
+  const l = r.length;
+  console.log(`l: ${l}`); // 3
 }
 
 {
-  let r: Hello;
-  // r = 5; // NOT ASSIGNABLE
-  // r += 1; // NOT APPLIED
+  let r;
   {
     const x = { hello: 'world' };
     r = x;
